ff5d3599bef7ccd5d18382afece5c18e0935e496
PranavYugan <yuganpranav@gmail.com>
2026-04-17 14:46:24 +0530

    create OrderItemRequestDTO for incoming order item data

---
+++ src/main/java/com/seveneleven/storeapp/model/dto/OrderItemRequestDTO.java
@@ -0,0 +1,25 @@
+package com.seveneleven.storeapp.model.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItemRequestDTO {
+    @NotNull(message = "Product id is required")
+    private Long productId;
+
+    @Positive(message = "Quantity must be greater than zero")
+    private int quantity;
+
+    @NotNull(message = "Unit price is required")
+    @DecimalMin(value = "0.0", inclusive = true, message = "Unit price must be non-negative")
+    private Double unitPrice;
+}
